ff14b67c63363e4efe3b01040962d91e58ac1b37
Soheil Ansaari <72976010+so-heil@users.noreply.github.com>
2023-12-24 21:44:34 +0330

    fix: send proper Content-Type header and refactor

--- foundation/notion/notion.go
+++ foundation/notion/notion.go
@@ -1,3 +1,4 @@
+// Package notionprovider provides a client to call notionprovider API
 package notion
 
 import (
@@ -7,26 +8,31 @@ import (
 	"net/http"
 )
 
+// Client is a notionprovider client that can send requests to notionprovider API
 type Client struct {
 	apiKey        string
 	version       string
 	notionVersion string
 }
 
+const APIVersion = "v1"
+const Version = "2022-06-28"
+const Base = "https://api.notion.com"
+
+// NewClient creates a new notionprovider client with default versions set
 func NewClient(apiKey string) *Client {
 	return &Client{
 		apiKey:        apiKey,
-		version:       "v1",
-		notionVersion: "2022-06-28",
+		version:       APIVersion,
+		notionVersion: Version,
 	}
 }
 
+// NotionError represents an error that notionprovider API responded with when called
 type NotionError struct {
-	Object    string `json:"object"`
-	Status    int    `json:"status"`
-	Code      string `json:"code"`
-	Message   string `json:"message"`
-	RequestId string `json:"request_id"`
+	Status  int    `json:"status"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
 }
 
 func (ne NotionError) Error() string {
@@ -34,43 +40,32 @@ func (ne NotionError) Error() string {
 }
 
 func (c *Client) Request(method string, path string, body io.Reader, value any) error {
-	url := fmt.Sprintf("https://api.notion.com/%s%s", c.version, path)
+	url := fmt.Sprintf("%s/%s%s", Base, c.version, path)
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return fmt.Errorf("request contruction for url: %s: %w", url, err)
+		return fmt.Errorf("construct construct notionprovider request: url: %s: %w", url, err)
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.apiKey))
 	req.Header.Add("Notion-Version", c.notionVersion)
+	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("request failed for url: %s: %w", url, err)
+		return fmt.Errorf("notionprovider request: url: %s: %w", url, err)
 	}
 
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+	if resp.StatusCode >= http.StatusBadRequest {
 		var ne NotionError
 		if err := dec.Decode(&ne); err != nil {
 			return fmt.Errorf("decode error body response: %w", err)
 		}
-		return fmt.Errorf("unsuccessful request with status code %d: %w", resp.StatusCode, ne)
+		return fmt.Errorf("notionprovider request: status code %d: %w", resp.StatusCode, ne)
 	}
 
 	if err := dec.Decode(value); err != nil {
 		return fmt.Errorf("decode body response: %w", err)
-	}
-
-	return nil
-}
-
-type Blocks struct {
-	Type string
-}
-
-func (nc *Client) BlockChildren(blockID string, blocks Blocks) error {
-	if err := nc.Request(http.MethodGet, fmt.Sprintf("/blocks/%s/children?page_size=500", blockID), nil, &blocks); err != nil {
-		return fmt.Errorf("retiriving block with id %s childern: %w", blockID, err)
 	}
 
 	return nil
